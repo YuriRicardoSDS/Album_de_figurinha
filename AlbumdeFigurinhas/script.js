ae6170f1d01d4742ef3de3c33bc75ee25c49ffe1
YuriRicardoSDS <166342523+YuriRicardoSDS@users.noreply.github.com>
2026-03-09 14:33:49 -0300

    Delete AlbumdeFigurinhas directory

--- AlbumdeFigurinhas/script.js
+++
@@ -1,5 +0,0 @@
-function mostrarTexto(elemento){
-  const todos = document.querySelectorAll('.img-container');
-  todos.forEach(el => el.classList.remove('selecionado'));
-  elemento.classList.add('selecionado');
-}
